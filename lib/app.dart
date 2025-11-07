7a1a0f3a119faa9fc52a7b26000377ab124f3ada
zersix225 <drago.overlond@gmail.com>
2025-11-08 03:37:03 +0700

    chore: Added shadcn ui dependency

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/features/screens/home.dart';
-import 'package:todo_list/utils/theme/theme.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 
 class MyApp extends StatelessWidget {
@@ -8,11 +7,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-      home: const HomeScreen(),
+    return ShadApp.custom(
+      themeMode: ThemeMode.dark,
+      darkTheme: ShadThemeData(
+        brightness: Brightness.dark,
+        colorScheme: const ShadSlateColorScheme.dark(),
+      ),
+      appBuilder: (context) {
+        return MaterialApp(
+          theme: Theme.of(context),
+          builder: (context, child) {
+            return ShadAppBuilder(child: child!);
+          },
+        );
+      },
     );
   }
+
 }
